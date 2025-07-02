7cff15e8657930d59e0a54aecba1501aca1aec22
lnash2 <lewis@crimsonfox.co.uk>
2025-07-02 12:57:41 +0100

    Re-add SQL files without Git LFS

---
+++ leg-sql/legacy_data.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a7e0a6f24bf57d1a889453dc3bfddbb5a04b6b7428b3efc1c2cbebfd57b46d7b
+size 624483724
---
+++ leg-sql/legacy_schema.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7e63a9e97efed27c96978b1f1d7d4de2826db0ad7c1d745ef2cf53736079e4eb
+size 125454
